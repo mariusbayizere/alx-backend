b6276ae22072cb5e7e5b87b69ad8833d55c603c1
mariusbayizere <bayizeremarius119@gmail.com>
2024-08-27 21:52:27 +0200

    push 0x02. i18n To git

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
     The root route renders the index.html template.
     """
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
